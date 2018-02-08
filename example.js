037a53e4e432e81a611e5b6b4df3b336712d9b17
Avinash Anand <avinash@ledgerex.io>
2018-02-08 19:58:57 +0530

    example modified

--- example.js
+++ example.js
@@ -8,6 +8,6 @@ var response = CustomRes.createResponse().httpCode(303).message(msg).success(fal
 var err = CustomRes.createCustomError(response).message(msg2)
 var err2 = CustomRes.castErr('This is error');
 var err3 = CustomRes.castErr(new Error('error type')).get();
-var err4 = CustomRes.castErrNSend(status2)
+var err4 = CustomRes.castErr(status2)
 
 console.log(err, err2, err3, err4);
